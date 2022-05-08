aadb57fcfde3a76effd53a9d656e03eb373bb768
kia <dont@email.com>
2022-05-08 17:29:55 -0400

    implement firebase

--- dbWorker.js
+++ dbWorker.js
@@ -1,1 +1,21 @@
-//need to download firebase and get it setup for next steps on the project
+const { parentPort } = require('worker_threads');
+const admin = require('firebase-admin');
+var serviceAccount = require("./spidey-firebase-adminsdk.json");
+
+admin.initializeApp({
+credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
+
+let date = new Date();
+let currDate = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+
+parentPort.once("message", (message) => {
+    console.log('Received data from mainWorker..');
+    db.collection('Rates').doc(currDate).set({
+        rates: JSON.stringify(message)
+    }).then(() => {
+        parentPort.postMessage('Data saved successfulllly');
+    }).catch(err => console.log(err))
+});
